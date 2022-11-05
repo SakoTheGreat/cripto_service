243ed4c3565ad79b90b2b5485a50e094010125a2
Andrei Sakovich <sakowlandrei@mail.ru>
2022-11-05 14:02:04 +0100

    Функция бинанса и заполение бazi

--- get_all_binance.py
+++ get_all_binance.py
@@ -1,9 +1,9 @@
 from webapp import create_app
-from webapp.function_date_binance import fill_binance_data_table
-from webapp.function_date_bybit import fill_bybit_data_table
+from webapp.function_date_binance import proba_pera
+#from webapp.function_date_bybit import fill_bybit_data_table
 
 
 app = create_app()
 with app.app_context():
-    fill_binance_data_table()
-    fill_bybit_data_table()
+    proba_pera()
+    #fill_bybit_data_table()
--- webapp/function_date_binance.py
+++ webapp/function_date_binance.py
@@ -1,53 +1,58 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from time import sleep
-
+from selenium.common.exceptions import NoSuchElementException
 from webapp.model import db, BitCurrency
 
 
-action_types ={
-    'Sell': 'sell',
-    'Buy': '',
-}
-
-tokens = ['USDT', 'BTC']
-payment_methods = {
-    'Tinkoff': 'TinkoffNew',
-    'RosBank': 'RosBankNew',
-    'RaiffeisenBank': 'RaiffeisenBank',
-}
 
 def get_price(sub_url):
     driver = Chrome()
     driver.get(f"https://p2p.binance.com/ru/trade/{sub_url}")
     sleep(6)
     search_price = driver.find_element(By.CSS_SELECTOR, '[class="css-1m1f8hn"]')
-    driver.close()
-    return search_price.text
+    price = search_price.text
+    return price
 
 def save_binance(all_prices):
     for price_info in all_prices:
-        bin_bin = BitCurrency(sell_purchase=price_info[1], currency=price_info[2], bank=price_info[3], site=price_info[4], price=price_info[0])
+        bin_bin = BitCurrency(sell_purchase=price_info[1], currency=price_info[3], bank=price_info[4], site=price_info[-1], price=price_info[0])
         db.session.add(bin_bin)
         db.session.commit()
 
 
 def proba_pera():
+    action_types ={
+    'Sell': 'sell',
+    'Buy': '',
+}
+
+    tokens = ['USDT', 'BTC']
+    payment_methods = {
+    'Tinkoff': 'TinkoffNew',
+    'RosBank': 'RosBankNew',
+    'RaiffeisenBank': 'RaiffeisenBank',
+}
+    pp = 'RUB'
     all_prices = []
     for token in tokens:
         for bank, bank_id in payment_methods.items():
             for action, action_id in action_types.items():
                 price_info = []
-                key = (action, token, 'RUB', bank, 'Binance')
+                key = (action, token, pp, bank, 'Binance')
+                try:
+                    if action == 'Sell':
+                        price = get_price(f"{action_id}/{token}?fiat={pp}&payment={bank_id}")
+                        price_info.append(price)
+                    elif action == 'Buy':
+                        price = get_price(f"{bank_id}/{token}?fiat={pp}")
+                        price_info.append(price)                        
+                except NoSuchElementException:
+                    continue
                 for i in key:
                     price_info.append(i)
-                if action == 'Sell':
-                    price = get_price(f"{action_id}/{token}?fiat=RUB&payment={bank_id}")
-                    price_info.append(price)
-                elif action == 'Buy':
-                    price = get_price(f"{bank_id}/{token}?fiat=RUB")
-                    price_info.append(price)
-            
-                all_prices.append(price_info)
-            
-    save_binance(all_prices)
+                all_prices.append(price_info)    
+
+    save_binance(all_prices)    
+    
+
